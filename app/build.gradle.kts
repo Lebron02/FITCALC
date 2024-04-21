7cb81354fc2133b7d44c6ab4e8c55bafb2e5ec9d
Bartomiej Król <krolbartlomiej1122@gmail.com>
2024-04-21 11:51:41 +0200

    stworzenie LoginActivity wrap z wyglądem

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 android {
     namespace = "com.example.fitcalc"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.fitcalc"
---
+++ app/src/main/java/com/example/fitcalc/LoginActivity.java
@@ -0,0 +1,41 @@
+package com.example.fitcalc;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class LoginActivity extends AppCompatActivity {
+
+    private EditText editTextLogin;
+    private EditText editTextPassword;
+    private Button buttonLogin;
+    private TextView textViewRegister;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        editTextLogin = findViewById(R.id.inputlogin);
+        editTextPassword = findViewById(R.id.inputpassword);
+        buttonLogin = findViewById(R.id.loginbutton);
+        textViewRegister = findViewById(R.id.textregister);
+
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Logika logowania
+            }
+        });
+
+        textViewRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Przejdź do aktywności rejestracji
+            }
+        });
+    }
+}
